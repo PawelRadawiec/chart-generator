0a0684f171d4ed20b12443df98857db42aa5fab0
PawelRadawiec <pawel.radaw@gmail.com>
2020-08-03 18:19:43 +0200

    file upload / store file

--- src/main/java/com/info/chartgenerator/controller/UploadController.java
+++ src/main/java/com/info/chartgenerator/controller/UploadController.java
@@ -1,8 +1,25 @@
 package com.info.chartgenerator.controller;
 
+import com.info.chartgenerator.model.FileResponse;
+import com.info.chartgenerator.service.UploadStorageService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class UploadController {
+
+    private UploadStorageService storageService;
+
+    public UploadController(UploadStorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @PostMapping(value = "/upload")
+    public FileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = storageService.storeFile(file);
+        return new FileResponse(fileName, "TYPE", "URI", file.getSize());
+    }
 
 }
---
+++ src/main/java/com/info/chartgenerator/service/UploadStorageService.java
@@ -0,0 +1,43 @@
+package com.info.chartgenerator.service;
+
+import com.info.chartgenerator.config.FileStorageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class UploadStorageService {
+
+    private final Path fileLocation;
+
+    @Autowired
+    public UploadStorageService(FileStorageProperties fileStorageProperties) {
+        this.fileLocation = Paths.get(fileStorageProperties.getUploadDir())
+                .toAbsolutePath()
+                .normalize();
+        try {
+            Files.createDirectories(this.fileLocation);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path targetLocation = this.fileLocation.resolve(fileName);
+        try {
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileName;
+    }
+
+}
