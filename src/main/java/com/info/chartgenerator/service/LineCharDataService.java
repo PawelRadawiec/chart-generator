8ca82f40d157e9b2f3ea6f569e41556623e3b94c
PawelRadawiec <pawel.radaw@gmail.com>
2020-11-14 15:56:29 +0100

    chart line / fixed data and labels

--- src/main/java/com/info/chartgenerator/service/LineCharDataService.java
+++ src/main/java/com/info/chartgenerator/service/LineCharDataService.java
@@ -9,50 +9,63 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Service
 @SuppressWarnings("WeakerAccess")
 public class LineCharDataService {
 
+
     public ChartData generateLineChartData(Sheet sheet) {
         ChartData chartData = new ChartData();
         List<ChartDataSet> chartDataSets = chartData.getChartDataSet();
+        List<String> lineChartLabels = new ArrayList<>();
+        int rowIndex = 0;
+        int cellCounter = 0;
+        boolean firstRowAsLabels = false;
         for (Row row : sheet) {
-            int columnIndex = 0;
+            ChartDataSet rowData = new ChartDataSet();
+            List<Double> rowDataValue = new ArrayList<>();
             for (Cell cell : row) {
-                ChartDataSet chartDataSet;
-                if (columnIndex >= chartDataSets.size()) {
-                    chartDataSet = new ChartDataSet();
-                    chartDataSet.setLabel("column " + columnIndex);
-                    chartDataSets.add(columnIndex, chartDataSet);
-                } else {
-                    chartDataSet = chartDataSets.get(columnIndex);
-                }
-                List<Double> data = chartDataSet.getData();
-                if (CellType.STRING.equals(cell.getCellType())) {
-                    chartDataSet.setLabel(cell.getStringCellValue());
-                } else {
-                    data.add(cell.getNumericCellValue());
-                }
-                chartDataSet.setData(data);
-                chartDataSets.set(columnIndex, chartDataSet);
-                columnIndex++;
+                firstRowAsLabels = (rowIndex == 0 && cellCounter == 0 && CellType.STRING.equals(cell.getCellType()));
+                appendLineChartLabels(cell, lineChartLabels, rowIndex);
+                appendRowData(cell, rowData, rowDataValue, rowIndex);
+                cellCounter++;
             }
+            rowData.setData(rowDataValue);
+            if (!firstRowAsLabels && rowIndex == 0) {
+                chartDataSets.add(rowData);
+            }
+            if (rowIndex > 0)
+                chartDataSets.add(rowData);
+            rowIndex++;
         }
-        chartData.setChartDataSet(chartDataSets);
-        chartData.setLineChartLabels(generateLineChartLabels(sheet));
+        chartData.setChartDataSet(chartDataSets
+                .stream()
+                .filter(data -> !data.getData().isEmpty())
+                .collect(Collectors.toList())
+        );
+        chartData.setLineChartLabels(lineChartLabels);
         chartData.setType(ChartType.LINE);
         return chartData;
     }
 
-    private List<String> generateLineChartLabels(Sheet sheet) {
-        return IntStream.range(0, sheet.getPhysicalNumberOfRows())
-                .boxed()
-                .map(String::valueOf)
-                .collect(Collectors.toList());
+    private void appendLineChartLabels(Cell cell, List<String> lineChartLabels, int rowIndex) {
+        boolean stringType = CellType.STRING.equals(cell.getCellType());
+        if (stringType && rowIndex == 0) {
+            lineChartLabels.add(cell.getStringCellValue());
+        } else if (!stringType && rowIndex == 0) {
+            lineChartLabels.add(String.valueOf(cell.getColumnIndex()));
+        }
+    }
+
+    private void appendRowData(Cell cell, ChartDataSet rowData, List<Double> rowDataValue, int rowIndex) {
+        if (CellType.NUMERIC.equals(cell.getCellType())) {
+            rowDataValue.add(cell.getNumericCellValue());
+            rowData.setLabel("Series " + rowIndex);
+        }
     }
 
 }
